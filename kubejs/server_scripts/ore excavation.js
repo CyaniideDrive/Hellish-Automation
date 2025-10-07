d3a2c8120bc9ad73c383e46f1db4354fdee45be0
CyanSoup <daniellboivin@gmail.com>
2025-10-07 12:59:09 -0400

    Minor update.
    Added Sodium Extras, Equipment Compare and Corpse.
    
    Also decreased ore vein rarity majorly.

--- kubejs/server_scripts/ore excavation.js
+++ kubejs/server_scripts/ore excavation.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
 
 	event.remove({ id:'createoreexcavation:ore_vein_type/glowstone'})
 	event.remove({ id:'createoreexcavation:drilling/glowstone'})
-	event.recipes.createoreexcavation.vein('{"text": "Glowstone"}', 'minecraft:glowstone_dust').id('kubejs:glowstone').biomeWhitelist('minecraft:is_nether').placement(64, 32, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Glowstone"}', 'minecraft:glowstone_dust').id('kubejs:glowstone').biomeWhitelist('minecraft:is_nether').placement(64, 16, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		['2x minecraft:glowstone_dust', Item.of('2x minecraft:glowstone_dust').withChance(0.5)],
@@ -14,7 +14,7 @@ ServerEvents.recipes(event => {
 
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Lapis"}', 'minecraft:lapis_lazuli').id('kubejs:nether_lapis').biomeWhitelist('minecraft:is_nether').placement(256, 16, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Lapis"}', 'minecraft:lapis_lazuli').id('kubejs:nether_lapis').biomeWhitelist('minecraft:is_nether').placement(256, 24, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		['minecraft:lapis_lazuli', Item.of('6x minecraft:lapis_lazuli').withChance(0.5)],
@@ -26,7 +26,7 @@ ServerEvents.recipes(event => {
 
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Coal"}', 'minecraft:coal').id('kubejs:nether_coal').biomeWhitelist('minecraft:is_nether').placement(128, 64, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Coal"}', 'minecraft:coal').id('kubejs:nether_coal').biomeWhitelist('minecraft:is_nether').placement(128, 8, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		'minecraft:lapis_coal',
@@ -37,7 +37,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_coal2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Apatite"}', 'thermal:apatite').id('kubejs:nether_apatite').biomeWhitelist('minecraft:is_nether').placement(256, 32, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Apatite"}', 'thermal:apatite').id('kubejs:nether_apatite').biomeWhitelist('minecraft:is_nether').placement(256, 12, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		['2x thermal:apatite', Item.of('6x thermal:apatite').withChance(0.5)],
@@ -48,7 +48,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_apatite2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Sulfur"}', 'thermal:sulfur').id('kubejs:nether_sulfur').biomeWhitelist('minecraft:is_nether').placement(512, 16, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Sulfur"}', 'thermal:sulfur').id('kubejs:nether_sulfur').biomeWhitelist('minecraft:is_nether').placement(512, 12, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:sulfur',
@@ -59,7 +59,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_sulfur2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Niter"}', 'thermal:niter').id('kubejs:nether_niter').biomeWhitelist('minecraft:is_nether').placement(512, 32, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Niter"}', 'thermal:niter').id('kubejs:nether_niter').biomeWhitelist('minecraft:is_nether').placement(512, 12, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:niter',
@@ -71,7 +71,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_niter2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Cinnabar"}', 'thermal:cinnabar').id('kubejs:nether_cinnabar').biomeWhitelist('minecraft:is_nether').placement(512, 64, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Cinnabar"}', 'thermal:cinnabar').id('kubejs:nether_cinnabar').biomeWhitelist('minecraft:is_nether').placement(512, 12, 6856894)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:cinnabar',
@@ -85,7 +85,7 @@ ServerEvents.recipes(event => {
 
 	event.remove({ id:'createoreexcavation:drilling/quartz'})
 	event.remove({ id:'createoreexcavation:ore_vein_type/quartz'})
-	event.recipes.createoreexcavation.vein('{"text": "Nether Quartz"}', 'minecraft:quartz').id('kubejs:nether_quartz').biomeWhitelist('minecraft:is_nether').placement(64, 16, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Quartz"}', 'minecraft:quartz').id('kubejs:nether_quartz').biomeWhitelist('minecraft:is_nether').placement(64, 12, 6869485)
 
 	event.recipes.createoreexcavation.drilling(
 		['minecraft:quartz', Item.of('minecraft:quartz').withChance(0.25)],
@@ -97,7 +97,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_quartz2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Certus Quartz"}', 'ae2:certus_quartz_crystal').id('kubejs:nether_certus_quartz').biomeWhitelist('minecraft:is_nether').placement(64, 512, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Certus Quartz"}', 'ae2:certus_quartz_crystal').id('kubejs:nether_certus_quartz').biomeWhitelist('minecraft:is_nether').placement(64, 12, 9468685)
 
 	event.recipes.createoreexcavation.drilling(
 		['ae2:certus_quartz_crystal', Item.of('ae2:charged_certus_quartz_crystal').withChance(0.25)],
@@ -109,7 +109,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_certus_quartz2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Copper"}', 'minecraft:raw_copper').id('kubejs:nether_copper').biomeWhitelist('minecraft:is_nether').placement(1024, 16, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Copper"}', 'minecraft:raw_copper').id('kubejs:nether_copper').biomeWhitelist('minecraft:is_nether').placement(512, 16, 6868945)
 
 	event.recipes.createoreexcavation.drilling(
 		['2x minecraft:raw_copper', Item.of('4x minecraft:raw_copper').withChance(0.5)],
@@ -121,7 +121,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_copper2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Iron"}', 'minecraft:raw_iron').id('kubejs:nether_iron').biomeWhitelist('minecraft:is_nether').placement(1024, 64, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Iron"}', 'minecraft:raw_iron').id('kubejs:nether_iron').biomeWhitelist('minecraft:is_nether').placement(512, 24, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		'minecraft:raw_iron',
@@ -133,7 +133,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_iron2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Zinc"}', 'create:raw_zinc').id('kubejs:nether_zinc').biomeWhitelist('minecraft:is_nether').placement(1024, 32, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Zinc"}', 'create:raw_zinc').id('kubejs:nether_zinc').biomeWhitelist('minecraft:is_nether').placement(512, 24, 6868945)
 
 	event.recipes.createoreexcavation.drilling(
 		'create:raw_zinc',
@@ -144,7 +144,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_zinc2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Tin"}', 'thermal:raw_tin').id('kubejs:nether_tin').biomeWhitelist('minecraft:is_nether').placement(1024, 32, 6885946)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Tin"}', 'thermal:raw_tin').id('kubejs:nether_tin').biomeWhitelist('minecraft:is_nether').placement(512, 24, 6885946)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:raw_tin',
@@ -156,7 +156,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_tin2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Lead"}', 'thermal:raw_lead').id('kubejs:nether_lead').biomeWhitelist('minecraft:is_nether').placement(1024, 32, 9468685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Lead"}', 'thermal:raw_lead').id('kubejs:nether_lead').biomeWhitelist('minecraft:is_nether').placement(512, 24, 6694885)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:raw_lead',
@@ -182,7 +182,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_gold2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Rich Gold"}', 'minecraft:raw_gold_block').id('kubejs:rich_nether_gold').biomeWhitelist('minecraft:is_nether').placement(512, 8, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Rich Gold"}', 'minecraft:raw_gold_block').id('kubejs:rich_nether_gold').biomeWhitelist('minecraft:is_nether').placement(512, 16, 8946685)
 
 	event.recipes.createoreexcavation.drilling(
 		[Item.of('8x minecraft:raw_gold_block'), Item.of('16x minecraft:raw_gold_block').withChance(0.5), Item.of('128x minecraft:raw_gold').withChance(0.5)],
@@ -194,7 +194,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:rich_nether_gold2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Silver"}', 'thermal:raw_silver').id('kubejs:nether_silver').biomeWhitelist('minecraft:is_nether').placement(1024, 256, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Silver"}', 'thermal:raw_silver').id('kubejs:nether_silver').biomeWhitelist('minecraft:is_nether').placement(512, 40, 6894865)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:raw_silver',
@@ -206,7 +206,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_silver2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Nickel"}', 'thermal:raw_nickel').id('kubejs:nether_nickel').biomeWhitelist('minecraft:is_nether').placement(1024, 32, 6898465)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Nickel"}', 'thermal:raw_nickel').id('kubejs:nether_nickel').biomeWhitelist('minecraft:is_nether').placement(512, 24, 4658698)
 
 	event.recipes.createoreexcavation.drilling(
 		'thermal:raw_nickel',
@@ -218,7 +218,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_nickel2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Redstone"}', 'createoreexcavation:raw_redstone').id('kubejs:nether_redstone').biomeWhitelist('minecraft:is_nether').placement(256, 128, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Redstone"}', 'createoreexcavation:raw_redstone').id('kubejs:nether_redstone').biomeWhitelist('minecraft:is_nether').placement(256, 16, 6894685)
 
 	event.recipes.createoreexcavation.drilling(
 		['createoreexcavation:raw_redstone', Item.of('2x createoreexcavation:raw_redstone').withChance(0.5), Item.of('8x minecraft:redstone').withChance(0.75)],
@@ -230,7 +230,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_redstone2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Raw Cobalt"}', 'tconstruct:raw_cobalt').id('kubejs:raw_cobalt').biomeWhitelist('minecraft:is_nether').placement(1024, 256, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Raw Cobalt"}', 'tconstruct:raw_cobalt').id('kubejs:raw_cobalt').biomeWhitelist('minecraft:is_nether').placement(512, 32, 6648895)
 
 	event.recipes.createoreexcavation.drilling(
 		'tconstruct:raw_cobalt',
@@ -242,7 +242,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:raw_cobalt2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Diamond"}', 'createoreexcavation:raw_diamond').id('kubejs:nether_diamond').biomeWhitelist('minecraft:is_nether').placement(2048, 512, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Diamond"}', 'createoreexcavation:raw_diamond').id('kubejs:nether_diamond').biomeWhitelist('minecraft:is_nether').placement(512, 64, 6468895)
 
 	event.recipes.createoreexcavation.drilling(
 		'createoreexcavation:raw_diamond',
@@ -254,7 +254,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_diamond2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Emerald"}', 'createoreexcavation:raw_emerald').id('kubejs:nether_emerald').biomeWhitelist('minecraft:is_nether').placement(2048, 512, 6868945)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Emerald"}', 'createoreexcavation:raw_emerald').id('kubejs:nether_emerald').biomeWhitelist('minecraft:is_nether').placement(512, 64, 6694588)
 
 	event.recipes.createoreexcavation.drilling(
 		'createoreexcavation:raw_emerald',
@@ -266,7 +266,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:nether_emerald2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Nether Arcane Debris"}', 'irons_spellbooks:arcane_debris').id('kubejs:nether_arcane_debris').biomeWhitelist('minecraft:is_nether').placement(2048, 1024, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Nether Arcane Debris"}', 'irons_spellbooks:arcane_debris').id('kubejs:nether_arcane_debris').biomeWhitelist('minecraft:is_nether').placement(512, 128, 9568684)
 
 	event.recipes.createoreexcavation.drilling(
 		'irons_spellbooks:arcane_debris',
@@ -280,7 +280,7 @@ ServerEvents.recipes(event => {
 
 	event.remove({ id:'createoreexcavation:ore_vein_type/netherite'})
 	event.remove({ id:'createoreexcavation:drilling/netherite'})
-	event.recipes.createoreexcavation.vein('{"text": "Poor Ancient Debris"}', 'minecraft:netherite_scrap').id('kubejs:ancient_debris').biomeWhitelist('minecraft:is_nether').placement(2048, 64, 6868594)
+	event.recipes.createoreexcavation.vein('{"text": "Poor Ancient Debris"}', 'minecraft:netherite_scrap').id('kubejs:ancient_debris').biomeWhitelist('minecraft:is_nether').placement(512, 32, 668589)
 
 	event.recipes.createoreexcavation.drilling(
 		['tconstruct:debris_nugget', Item.of('8x tconstruct:debris_nugget').withChance(0.5), Item.of('2x minecraft:netherite_scrap').withChance(0.5), Item.of('minecraft:ancient_debris').withChance(0.1)],
@@ -292,7 +292,7 @@ ServerEvents.recipes(event => {
 		.id('kubejs:ancient_debris2');
 
 
-	event.recipes.createoreexcavation.vein('{"text": "Rich Ancient Debris"}', 'minecraft:ancient_debris').id('kubejs:rich_ancient_debris').biomeWhitelist('minecraft:is_nether').placement(2048, 2048, 6894685)
+	event.recipes.createoreexcavation.vein('{"text": "Rich Ancient Debris"}', 'minecraft:ancient_debris').id('kubejs:rich_ancient_debris').biomeWhitelist('minecraft:is_nether').placement(512, 128, 8658946)
 
 	event.recipes.createoreexcavation.drilling(
 		[Item.of('2x minecraft:ancient_debris'), Item.of('4x minecraft:ancient_debris').withChance(0.5)],
